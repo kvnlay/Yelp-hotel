4cfca702a09d00046c8089f20629fda264b780ae
EC2 Default User <ec2-user@ip-172-31-37-135.us-east-2.compute.internal>
2019-09-16 22:14:41 +0000

    update heroku cli and add gitignore

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect(process.env.databaseURL);
+mongoose.connect(process.env.databaseURL, { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
